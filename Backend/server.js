7d3b6eaa8b70fa04680c201ad7f4577236b89ea2
Vishxlll20 <vishalk785032@gmail.com>
2026-05-21 17:14:54 +0530

    feat(auth): implement user registration system

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
+dotenv.config();
+
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
